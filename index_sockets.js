a746919d19cc752dc4e439cbb31b8e095f2a60e2
electronicRevival <nicholas.konrad@mail.de>
2020-02-07 16:56:30 +0100

    Updated index.js

--- index_sockets.js
+++ index_sockets.js
@@ -22,10 +22,9 @@ function getCoefficient_Pot(val) {
 function updateVideoPBR() {
   //http://fooplot.com/#W3sidHlwZSI6MCwiZXEiOiIwLjA2MjUtMTYoeC0xKSIsImNvbG9yIjoiIzAwMDAwMCJ9LHsidHlwZSI6MTAwMCwid2luZG93IjpbIi0xNi41NzEyMDAwMDAwMDAwMDUiLCIxNi43MDg4MDAwMDAwMDAwMDQiLCItMy44NTAzOTk5OTk5OTk5OTkiLCIxNi42Mjk2MDAwMDAwMDAwMDMiXX1d
   //f(x) = 0.0625 - 16(x-1)  |  x € [0;1]
-  // if (inoData.brake != (undefined || null)) video_pbr = MIN_PLAYBACKRATE - (MAX_PLAYBACKRATE * (getCoefficient_Pot(inoData.brake) - 1));
-  if ((inoData.distance/200) > 16) video_pbr = 16;
-  else if ((inoData.distance/200) < 0.0625) video_pbr = 0.0625;
-  else video_pbr = (inoData.distance/200)*16;
+  if ((inoData.distance/200) > MAX_PLAYBACKRATE) video_pbr = MAX_PLAYBACKRATE;
+  else if ((inoData.distance/200) < MIN_PLAYBACKRATE) video_pbr = MIN_PLAYBACKRATE;
+  else video_pbr = parseFloat((inoData.distance/200)*16);
 }
 
 var socket = io();
